0d8da2fd579bb09292abb6e810efe47834554319
Stefan <bichlmaier.stef@gmail.com>
2022-03-15 00:05:05 -0700

    add includes

--- udp_poc.h
+++ udp_poc.h
@@ -1,3 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <cmath>
+#include <string.h>
+#include <inttypes.h>
+#include <fstream>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+
+using std::string;
+using namespace std;
+
+#include "autopilot_interface.h"
 #include <common/mavlink.h>
 #include "udp_port.h"
 
